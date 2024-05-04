4c41d166c59dc3b4c321048bcbcbe424f7a892da
TAHIR WAQAS <147711205+tahirwaqas7@users.noreply.github.com>
2024-05-04 12:19:17 +0500

    gsap

--- 2/script.js
+++ 2/script.js
@@ -1,6 +1,20 @@
-// import { gsap } from "gsap";
-
 gsap.from("#page1 #box", {
   scale: 0,
-  duration: 1,
+  delay: 1,
+  duration: 2,
+  rotate: 360,
+});
+
+gsap.from("#page2 #box", {
+  scale: 0,
+  delay: 1,
+  duration: 2,
+  rotate: 360,
+});
+
+gsap.from("#page3 #box", {
+  scale: 0,
+  delay: 1,
+  duration: 2,
+  rotate: 360,
 });
